ce2dba3b807ba86f21379b7b41c66a1844ffcdd3
Sam Whitlock <433170+samwhitlock@users.noreply.github.com>
2024-02-19 15:39:32 -0800

    bumping cpm version

--- src/Bootstrap.cmake
+++ src/Bootstrap.cmake
@@ -2,7 +2,7 @@ include_guard()
 
 include(FetchContent)
 
-set(CPM_VERSION "v0.38.6")
+set(CPM_VERSION "v0.38.7")
 FetchContent_Declare(
   cpm
   GIT_REPOSITORY https://github.com/cpm-cmake/CPM.cmake.git

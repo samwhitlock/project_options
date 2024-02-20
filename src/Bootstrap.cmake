a76eab1a03a6995f3b0c6859cfbfd98f9721a23d
Sam Whitlock <433170+samwhitlock@users.noreply.github.com>
2024-02-19 16:15:54 -0800

    Only bootstrapping cpm if needed

--- src/Bootstrap.cmake
+++ src/Bootstrap.cmake
@@ -1,11 +1,14 @@
 include_guard()
 
-include(FetchContent)
+if (NOT DEFINED cpm_SOURCE_DIR)
+  include(FetchContent)
 
-set(CPM_VERSION "v0.38.7")
-FetchContent_Declare(
-  cpm
-  GIT_REPOSITORY https://github.com/cpm-cmake/CPM.cmake.git
-  GIT_TAG ${CPM_VERSION})
-FetchContent_Populate(cpm)
+  set(CPM_VERSION "v0.38.7")
+  FetchContent_Declare(
+    cpm
+    GIT_REPOSITORY https://github.com/cpm-cmake/CPM.cmake.git
+    GIT_TAG ${CPM_VERSION})
+  FetchContent_Populate(cpm)
+endif()
+
 include(${cpm_SOURCE_DIR}/cmake/CPM.cmake)
